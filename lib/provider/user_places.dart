8c15081631e351d5887de43d7a07c09350d9b45b
Xiosmilus <ysgupta110@gmail.com>
2023-08-07 09:14:35 +0530

    Basic Places App

---
+++ lib/provider/user_places.dart
@@ -0,0 +1,13 @@
+import 'package:favourite_places/models/place.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class UserPlacesNotifier extends StateNotifier<List<Place>> {
+  UserPlacesNotifier() : super(const []);
+
+  void addPlace(String title) {
+    final newPlace = Place(title: title);
+    state = [...state, newPlace];
+  }
+}
+
+final userPlaceProvider = StateNotifierProvider<UserPlacesNotifier,List<Place>>((ref) => UserPlacesNotifier());
